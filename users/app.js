ed9037b2a7c66c42d50f4360764eb8d43da54515
rahullahoria <rahul_lahoria@yahoo.com>
2021-05-07 14:53:49 +0530

    mogiio/pwr-users:v1.05

--- users/app.js
+++ users/app.js
@@ -44,6 +44,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", require('./routes/users/'));
+app.use("/webinars", require('./routes/webinars/'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
